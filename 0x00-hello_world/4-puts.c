32ae6451121a2e3fff30eb1ae5cf17d3677d6c5b
Rapsnader <orapelengmorakile12@gmail.com>
2022-09-12 02:46:06 -0700

    1st c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Main - displaying using the put function 
+ *
+ * return will be 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
