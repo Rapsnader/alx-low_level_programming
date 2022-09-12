dac20974b14178c3a7c2e81745179f43afc8a587
Rapsnader <orapelengmorakile12@gmail.com>
2022-09-12 06:55:10 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the fprintf function
+*
+* Description: using the function main
+* return : 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
