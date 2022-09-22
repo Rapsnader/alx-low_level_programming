fc5884a96c79e43734036c33061c517d6faa094e
Rapsnader <orapelengmorakile12@gmail.com>
2022-09-22 13:28:21 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*_strcat - has the string pointed to @src to
+*the end of string point to dest @dest
+*@dest: string that will append
+@src: string to be concatenated
+*
+*Return:return pointer to dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for(index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
